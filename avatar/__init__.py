9b11c11d2cf86c33ee9c77aeb2d13f11f10b950f
Mr. 42 <johan@mr42.me>
2022-10-05 10:20:26 +0200

    Added __red_end_user_data_statement__

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -2,6 +2,10 @@
 import discord
 from .avatar import Avatar
 
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
 async def setup(bot):
     if discord.version_info.major == 2:
         await bot.add_cog(Avatar())
--- youtube/__init__.py
+++ youtube/__init__.py
@@ -2,6 +2,10 @@
 import discord
 from .youtube import YouTube
 
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
 async def setup(bot):
     if discord.version_info.major == 2:
         await bot.add_cog(YouTube(bot))
