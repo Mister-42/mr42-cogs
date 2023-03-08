c878d4ad6900ba59ab72bc793033a6c677fd73f6
Mr. 42 <johan@mr42.me>
2023-03-08 22:57:51 +0100

    [youtube] Added support for /shorts/ URLs

--- ytdedup/ytdedup.py
+++ ytdedup/ytdedup.py
@@ -154,6 +154,7 @@ class YouTubeDeDup(commands.Cog):
         if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
             if query.path == '/watch': return parse_qs(query.query)['v'][0]
             if query.path[:7] == '/watch/': return query.path.split('/')[1]
+            if query.path[:8] == '/shorts/': return query.path.split('/')[2]
             if query.path[:6] == '/live/': return query.path.split('/')[2]
             if query.path[:7] == '/embed/': return query.path.split('/')[2]
             if query.path[:3] == '/v/': return query.path.split('/')[2]
