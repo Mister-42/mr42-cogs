381be0c85dd5797ddd29eb86817c9d6951cb3d94
Mr. 42 <johan@mr42.me>
2023-11-14 21:02:34 +0100

    [youtube] Don't preview links from failed retrieves

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -721,7 +721,7 @@ class YouTube(commands.Cog):
 			with yt_dlp.YoutubeDL(options) as ydl, suppress(Exception):
 				return ydl.extract_info(url, download=False).get('channel_id')
 
-		await ctx.send(error(_("Unable to retrieve channel id from {channel}.").format(channel=bold(channelYouTube))))
+		await ctx.send(error(_("Unable to retrieve channel id from {channel}.").format(channel=bold(f"<{url}>"))))
 
 	async def subscription_discord_options(self, ctx: discord.abc.Messageable, action: str, channelYouTube: str, data: Optional[str], channelDiscord: Optional[discord.TextChannel] = None) -> None:
 		"""Store custom options for Discord channels."""
